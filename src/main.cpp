afb4a28f898b8360cc23c0b3d48e2d4ef4aac526
lkb133 <1327414288@qq.com>
2025-05-29 00:22:09 +0800

    websocket服务器更新

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include "WebSocket.h"
 
 const char *SSID = "HUAWEI";
 const char *PASSWORD = "88888888";
@@ -10,6 +11,16 @@ void setup()
   Serial.begin(115200);
   delay(500);
   initWiFi();
+  static httpd_handle_t server = null;
+  server = start_webserver();
+  
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
 }
 
 void loop()
